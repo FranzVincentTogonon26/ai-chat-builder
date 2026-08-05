8dab94c0fa6a07eb0dcfe320d66cad9fd73e5d17
FranzVincentTogonon26 <franzvincenttogonon@gmail.com>
2026-08-05 22:09:42 +0800

    feature: setup project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
@@ -19,11 +15,28 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en">
+      <body
+        className={`${inter.variable} bg-[#050509] min-h-screen flex flex-col  p-0 antialiased text-zinc-100 font-sans`}
+      >
+        <div className="fixed inset-0 -z-20 pointer-events-none">
+          <div className="w-full h-full opacity-20">
+            <div
+              data-us-project="franzvincenttogonon"
+              className="absolute w-full h-full left-0 top-0"
+            />
+            <div className="absolute inset-0 bg-linear-to-t from-[#050509] via-transparent to-transparent opacity-80" />
+          </div>
+        </div>
+        {children}
+        <Script
+          id="unicorn-studio"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `!function(){if(!window.UnicornStudio){window.UnicornStudio={isInitialized:!1};var i=document.createElement("script");i.src="https://cdn.jsdelivr.net/gh/hiunicornstudio/unicornstudio.js@v1.4.29/dist/unicornStudio.umd.js",i.onload=function(){window.UnicornStudio.isInitialized||(UnicornStudio.init(),window.UnicornStudio.isInitialized=!0)},(document.head || document.body).appendChild(i)}}();`,
+          }}
+        />
+      </body>
     </html>
   );
 }
