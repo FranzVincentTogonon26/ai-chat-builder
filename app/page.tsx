d9d83c064d5f93722e0f82a28e698e7bafd8b039
FranzVincentTogonon26 <franzvincenttogonon@gmail.com>
2026-08-05 22:42:21 +0800

    feature: build navbar in landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Navbar from "@/components/landing";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <main className="w-full flex flex-col relative z-10">
+      <Navbar />
+    </main>
+  );
 };
 
 export default page;
