85aa66b6b5b3e29ef8f7df92754d36660c4dadc9
FranzVincentTogonon26 <franzvincenttogonon@gmail.com>
2026-08-06 18:35:50 +0800

    feature: setup dashboard layout

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,20 @@
+import { cookies } from "next/headers";
+
+export const metadata = {
+  title: "AI Chatbot Support - Dashboard",
+  description:
+    "instantly resolve customer question with an assistant that reads your docs and speaks with empathy.",
+};
+
+export default async function DashboardLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  const cookieStore = await cookies();
+  const metadataCookie = cookieStore.get("metadata");
+
+  return (
+    <section className="bg-[#050509] min-h-screen font-sans antialiased text-zinc-100 selection:bg-zinc-800 flex">
+      {metadataCookie?.value ? <>{children}</> : children}
+    </section>
+  );
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import InitialForm from "@/components/dashboard/initial-from";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="flex-1 flex w-full">
+      <InitialForm />
+    </div>
+  );
+};
+
+export default Page;
